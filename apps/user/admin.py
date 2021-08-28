de5e1b95b11244617dfd929f52a94b90bca8fd22
mikolaj-czapp <mikolaj.czapp@gmail.com>
2021-08-28 12:42:20 +0200

    Admin, User changes
    Added Admin panel, User form and user modification from admin

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from apps.user.models import MyUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import MyUser
 
-admin.site.register(MyUser)
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = MyUser
+    list_display = ('email', 'city', 'street', 'postal_code', 'is_staff', 'is_active',)
+    list_filter = ('email', 'city', 'street', 'postal_code', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'city', 'street', 'postal_code')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'city', 'street', 'postal_code', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email', 'city', 'street', 'postal_code',)
+    ordering = ('email', 'city', 'street', 'postal_code',)
+
+
+admin.site.register(MyUser, CustomUserAdmin)
