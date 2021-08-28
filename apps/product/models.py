26d09004e1994f0e18e54c564ce2fc657b8fc854
mikolaj-czapp <mikolaj.czapp@gmail.com>
2021-08-28 12:46:49 +0200

    Brand model
    Added Brand model so that brands can be added

--- apps/product/models.py
+++ apps/product/models.py
@@ -8,18 +8,16 @@ class Category(models.Model):
         return self.name
 
 
+class Brand(models.Model):
+    brand = models.CharField(max_length=50)
+
+
 class Product(models.Model):
-    BRANDS = (
-        ('SX', 'SOXO'),
-        ('MAR', "MARLIN"),
-        ('MM', 'MANY MORNINGS')
-    )
     name = models.CharField(max_length=50)
-    brand = models.CharField(choices=BRANDS, max_length=50)
+    brand = models.ForeignKey(Brand, max_length=50)
     description = models.TextField(blank=True)
     category_id = models.ForeignKey(Category, default='Unbranded', on_delete=models.SET_DEFAULT)
     price = models.DecimalField(max_digits=12, decimal_places=2)
 
     def __str__(self):
         return f"{self.brand} - {self.name}"
-
