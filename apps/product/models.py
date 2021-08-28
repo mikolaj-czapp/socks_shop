e528bfba83905f04e5a7a2cbbb432ce8c7247425
mikolaj-czapp <mikolaj.czapp@gmail.com>
2021-08-28 13:02:20 +0200

    Added brand model
    Added brand model

--- apps/product/models.py
+++ apps/product/models.py
@@ -11,12 +11,14 @@ class Category(models.Model):
 class Brand(models.Model):
     brand = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.brand
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
-    brand = models.ForeignKey(Brand, max_length=50)
+    brand = models.ForeignKey(Brand, default='No brand', on_delete=models.CASCADE)
     description = models.TextField(blank=True)
-    category_id = models.ForeignKey(Category, default='Unbranded', on_delete=models.SET_DEFAULT)
+    category_id = models.ForeignKey(Category, default='No category', on_delete=models.SET_DEFAULT)
     price = models.DecimalField(max_digits=12, decimal_places=2)
 
     def __str__(self):
