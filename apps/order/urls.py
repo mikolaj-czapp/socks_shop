b566298e95615821f956dbceb1f42f994f8661d4
mikolaj-czapp <mikolaj.czapp@gmail.com>
2021-09-05 09:19:56 +0200

    Update urls.py

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -3,5 +3,6 @@ from apps.order import views
 from .views import CheckoutView
 
 urlpatterns = [
-    path('checkout/', CheckoutView.as_view(), name='checkout')
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('confirm_order', views.confirm_order, name='confirm_order'),
 ]
