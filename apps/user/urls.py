fd395fea5f875b7b96997ff5e27aa998eb8b9601
mikolaj-czapp <mikolaj.czapp@gmail.com>
2021-09-11 13:12:14 +0200

    added required fields in registration form

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -4,7 +4,7 @@ from apps.user.views import LoginView, RegisterView
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name = "register" ),
-    path('login/', LoginView.as_view(), name = "login" ),
-    path('logout/', LogoutView.as_view(), name = "logout" ),
+    path('register/', RegisterView.as_view(), name="register"),
+    path('login/', LoginView.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(), name="logout"),
 ]
