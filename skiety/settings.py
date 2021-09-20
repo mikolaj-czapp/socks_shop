e1294a2ef083aaaef9ba09171184df90c6638dbe
mikolaj-czapp <mikolaj.czapp@gmail.com>
2021-09-20 21:47:43 +0200

    Added some comments.

--- skiety/settings.py
+++ skiety/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-znmug3)4j_)tc+fj957_-g0yrt3$n%b7xjc@@n#w@(p*p0bz)8'
+SECRET_KEY = ''  # this is a secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
